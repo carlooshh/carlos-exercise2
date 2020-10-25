466c5d98e16e9c8e64a69a942c1d3d207d84ee0c
Carlos Henrique  Ribeiro dos Reis <carlos.henriquer287@gmail.com>
2020-10-25 16:13:15 -0300

    FIX: rename absolute path.

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/exercise2',
+    assetsPublicPath: '/carlos-exercise2',
 
     /**
      * Source Maps
