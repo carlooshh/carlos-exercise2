68c5a8bd7c5e228b6df56969248e8941759fb6f8
Carlos Henrique  Ribeiro dos Reis <carlos.henriquer287@gmail.com>
2020-10-25 16:24:29 -0300

    WIP: test directory.

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/carlos-exercise2',
+    assetsPublicPath: '/carlos-exercise',
 
     /**
      * Source Maps
