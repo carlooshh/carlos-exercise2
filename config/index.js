acb6899eba14ec0b323ec299ab01fe0d3883b355
Carlos Henrique  Ribeiro dos Reis <carlos.henriquer287@gmail.com>
2020-10-25 16:16:00 -0300

    FIX: added vue push dir.

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/carlos-exercise2',
+    assetsPublicPath: '/vue-push-dir',
 
     /**
      * Source Maps
